8f20656dc53d2400f3a53625d4406a755bd1800d
Adarsh Mallick <38970527+adaick@users.noreply.github.com>
2020-03-04 23:01:41 +0530

    Rail Shooter game.
    This is rail shooter game in  which enemies will fly on the specified path (called rail) the player have to shoot them or dodge them.
    Player can move verticle and horizontal and can shoot durig the movement.
    
    The core game loop is to fly as far as possible without dying in order to achieve the highest possible score.The game will restart after the player dies.

---
+++ Asault Shooter/Assets/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadSceneOne", 2f);
+    }
+
+    // Update is called once per frame
+    void LoadSceneOne()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
